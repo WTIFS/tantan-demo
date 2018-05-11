0f04f6e8f6c00f1282a369bf294f01b4f2e38c22
chenyuanfei <chenyuanfei@p1.com>
2018-05-11 21:11:15 +0800

    modify config

--- config/pg.go
+++ config/pg.go
@@ -2,7 +2,7 @@ package config
 
 //config of postgresSQL
 const (
-	PG_ADDRESS = "localhost:5432"
+	PG_ADDRESS = "127.0.0.1:5432"
 	PG_USER = "postgres"
 	PG_PASSWORD = "www"
 	PG_DATABASE = "putong"
